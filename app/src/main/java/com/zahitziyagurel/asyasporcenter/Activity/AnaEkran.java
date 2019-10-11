a4a4e8436e13864a6b8a9376a0baebc2b58efde9
Ziya <ziyagurel55@hotmail.com>
2019-10-11 22:19:01 +0300

    Initial commit

---
+++ app/src/main/java/com/zahitziyagurel/asyasporcenter/Activity/AnaEkran.java
@@ -0,0 +1,31 @@
+package com.zahitziyagurel.asyasporcenter.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.zahitziyagurel.asyasporcenter.R;
+
+public class AnaEkran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ana_ekran);
+
+        Button btn_satis = findViewById(R.id.btn_satis);
+        Button btn_stok = findViewById(R.id.btn_stok);
+        Button btn_rapor = findViewById(R.id.btn_rapor);
+        Button btn_kasa = findViewById(R.id.btn_kasa);
+
+        btn_satis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SatisEkrani.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/zahitziyagurel/asyasporcenter/Activity/SatisEkrani.java
@@ -0,0 +1,16 @@
+package com.zahitziyagurel.asyasporcenter.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zahitziyagurel.asyasporcenter.R;
+
+public class SatisEkrani extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_satis_ekrani);
+    }
+}
